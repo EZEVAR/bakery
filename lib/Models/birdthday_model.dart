5223877ad2af84e0c8b3490d65e4488d1e202c64
EZEVAR <ezequiel.vargas@hotmail.com>
2023-08-18 17:11:47 -0300

    update and create new pages and controller

---
+++ lib/Models/birdthday_model.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class BirthdayModel {
+  final String flavor;
+  final String price;
+  final color;
+  final String imagePath;
+  BirthdayModel(
+      {required this.flavor,
+      required this.price,
+      required this.imagePath,
+      required this.color});
+
+  static List<BirthdayModel> birthday = [
+    BirthdayModel(
+        flavor: 'Chocolate',
+        price: '\$2500',
+        color: Colors.brown,
+        imagePath: 'assets/birthdaycake/b3.jpeg'),
+    BirthdayModel(
+        flavor: 'Strawberry',
+        price: '\$2000',
+        color: Colors.red,
+        imagePath: 'assets/birthdaycake/b1.jpeg'),
+    BirthdayModel(
+        flavor: 'happy cake',
+        price: '\$1800',
+        color: Colors.purple,
+        imagePath: 'assets/birthdaycake/b2.jpeg'),
+    BirthdayModel(
+        flavor: 'Black Forest',
+        price: '\$2500',
+        color: Colors.green,
+        imagePath: 'assets/birthdaycake/b5.jpeg'),
+    BirthdayModel(
+        flavor: 'Raspberry',
+        price: '\$1800',
+        color: Colors.orange,
+        imagePath: 'assets/birthdaycake/b4.jpeg')
+  ];
+}
