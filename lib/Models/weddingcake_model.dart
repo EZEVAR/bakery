ae5dfe9018791a3ea095d77a652f9d08578c88df
Juan <juangrondona.01@gmail.com>
2023-08-25 17:50:22 -0300

    Agrego actualizaciones

---
+++ lib/Models/weddingcake_model.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class WeddingCakeModel {
+  final String flavor;
+  final String price;
+  final color;
+  final String imagePath;
+  WeddingCakeModel(
+      {required this.flavor,
+      required this.price,
+      required this.imagePath,
+      required this.color});
+
+  static List<WeddingCakeModel> wedding = [
+    WeddingCakeModel(
+        flavor: 'Wedding',
+        price: '\$290',
+        color: Colors.brown,
+        imagePath: 'assets/weddingcake/b1.jpg'),
+    WeddingCakeModel(
+        flavor: 'Reception',
+        price: '\$180',
+        color: Colors.red,
+        imagePath: 'assets/weddingcake/b2.jpg'),
+    WeddingCakeModel(
+        flavor: 'Pre-Wedding',
+        price: '\$245',
+        color: Colors.purple,
+        imagePath: 'assets/weddingcake/b3.jpg'),
+    WeddingCakeModel(
+        flavor: 'Engagement',
+        price: '\$100',
+        color: Colors.green,
+        imagePath: 'assets/weddingcake/b4.jpg'),
+    WeddingCakeModel(
+        flavor: 'Anversary',
+        price: '\$345',
+        color: Colors.orange,
+        imagePath: 'assets/weddingcake/b5.jpg')
+  ];
+}
--- lib/TabBarView/wedding_tab.dart
+++ lib/TabBarView/wedding_tab.dart
@@ -1,56 +1,50 @@
 import 'package:flutter/material.dart';
-
+import '../Models/weddingcake_model.dart';
 import '../Utilities/Tiles/weddingcake_tile.dart';
+import '../pages/weddingcake.dart';
 
 class WeddingCakeTab extends StatelessWidget {
-  const WeddingCakeTab({super.key});
+  const WeddingCakeTab({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    List weddingCakes = [
-      // flavor, price, color, image
-      [
-        'Wedding',
-        '\$290',
-        Colors.brown,
-        'assets/weddingcakes/wedding-cake1.png'
-      ],
-      [
-        'Reception',
-        '\$180',
-        Colors.red,
-        'assets/weddingcakes/wedding-cake2.png'
-      ],
-      [
-        'Pre-Wedding',
-        '\$245',
-        Colors.purple,
-        'assets/weddingcakes/wedding-cake3.png'
-      ],
-      [
-        'Engagement',
-        '\$100',
-        Colors.green,
-        'assets/weddingcakes/wedding-cake4.png'
-      ],
-      [
-        'Anversary',
-        '\$345',
-        Colors.orange,
-        'assets/weddingcakes/wedding-cake5.png'
-      ]
-    ];
+    List<WeddingCakeModel> weddings = WeddingCakeModel.wedding;
+    return WeddingCakeGrid(weddings: weddings);
+  }
+}
+
+class WeddingCakeGrid extends StatelessWidget {
+  const WeddingCakeGrid({
+    Key? key,
+    required this.weddings,
+  }) : super(key: key);
+
+  final List<WeddingCakeModel> weddings;
+
+  @override
+  Widget build(BuildContext context) {
     return GridView.builder(
-        itemCount: weddingCakes.length,
+        itemCount: weddings.length,
         padding: const EdgeInsets.all(12),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2, childAspectRatio: 1 / 1.5),
         itemBuilder: (context, index) {
-          return WeddingCakeTile(
-            flavor: weddingCakes[index][0],
-            price: weddingCakes[index][1],
-            colors: weddingCakes[index][2],
-            imagePath: weddingCakes[index][3],
+          WeddingCakeModel weddingcakeModel = weddings[index];
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          WeddingCakePage(wedding: weddingcakeModel)));
+            },
+            child: WeddingCakeTile(
+                flavor: weddingcakeModel.flavor,
+                imagePath: weddingcakeModel.imagePath,
+                price: weddingcakeModel.price,
+                colors: weddingcakeModel.color),
           );
         });
   }
