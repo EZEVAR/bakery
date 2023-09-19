4d9edf59ee1ddaf184d27f633a9b2bf2766bf0fb
EZEVAR <ezequiel.vargas@hotmail.com>
2023-09-19 16:01:10 -0300

    create auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:bakery/routes/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
