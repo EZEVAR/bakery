84c1b18654069cb34c9492a1317dbc67af85fbac
Juan <juangrondona.01@gmail.com>
2023-10-02 20:06:12 -0300

    Update sign_up_page

--- lib/sign_up/view/sign_up_page.dart
+++ lib/sign_up/view/sign_up_page.dart
@@ -14,12 +14,54 @@ class SignUpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Sign Up')),
-      body: Padding(
-        padding: const EdgeInsets.all(8),
-        child: BlocProvider<SignUpCubit>(
-          create: (_) => SignUpCubit(context.read<AuthenticationRepository>()),
-          child: const SignUpForm(),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.black,
+          image: DecorationImage(
+            image: const AssetImage('assets/fondo.jpg'),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+              Colors.black.withOpacity(0.5),
+              BlendMode.dstATop,
+            ),
+          ),
+        ),
+        padding: const EdgeInsets.all(25),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 150,
+                  width: 150,
+                  child: Image.asset("assets/icons/cupcakeicon.png"),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(bottom: 50),
+                  child: const Text(
+                    'Bakery',
+                    style: TextStyle(fontSize: 35, color: Colors.white),
+                  ),
+                ),
+                BlocProvider<SignUpCubit>(
+                  create: (_) =>
+                      SignUpCubit(context.read<AuthenticationRepository>()),
+                  child: const SignUpForm(),
+                ),
+                const SizedBox(height: 5), 
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text(
+                    'Already have an account? LOGIN',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
--- lib/theme.dart
+++ lib/theme.dart
@@ -3,9 +3,9 @@ import 'package:google_fonts/google_fonts.dart';
 
 final theme = ThemeData(
   textTheme: GoogleFonts.openSansTextTheme(),
-  primaryColorDark: Color.fromARGB(255, 84, 1, 82),
-  primaryColorLight: Color.fromARGB(255, 224, 115, 239),
-  primaryColor: Color.fromARGB(255, 248, 103, 154),
+  primaryColorDark: const Color.fromARGB(255, 84, 1, 82),
+  primaryColorLight: const Color.fromARGB(255, 224, 115, 239),
+  primaryColor: const Color.fromARGB(255, 248, 103, 154),
   colorScheme: const ColorScheme.light(secondary: Color(0xFF009688)),
   scaffoldBackgroundColor: const Color(0xFFE0F2F1),
   inputDecorationTheme: InputDecorationTheme(
