18d0e58f1abb3a72a6b9950a4c125d8ac2dd5743
Juan <juangrondona.01@gmail.com>
2023-10-04 11:46:15 -0300

    Update in Utilities

--- lib/Utilities/Tiles/cupcake_tile.dart
+++ lib/Utilities/Tiles/cupcake_tile.dart
@@ -29,76 +29,87 @@ class CupCakeTiles extends StatelessWidget {
             ),
           ],
         ),
-        child: Column(children: [
-          //Price
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Container(
-                padding: const EdgeInsets.all(12),
-                decoration: BoxDecoration(
-                  color: colors[100],
-                  borderRadius: const BorderRadius.only(
-                    topRight: Radius.circular(12),
-                    bottomLeft: Radius.circular(12),
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.8),
-                      spreadRadius: 0,
-                      blurRadius: 3,
-                      offset: const Offset(0, 2), // changes position of shadow
+        child: Column(
+          children: [
+            //Price
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    color: colors[100],
+                    borderRadius: const BorderRadius.only(
+                      topRight: Radius.circular(12),
+                      bottomLeft: Radius.circular(12),
                     ),
-                  ],
-                ),
-                child: Text(
-                  price,
-                  style: TextStyle(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.8),
+                        spreadRadius: 0,
+                        blurRadius: 3,
+                        offset:
+                            const Offset(0, 2), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: Text(
+                    price,
+                    style: TextStyle(
                       color: colors[800],
                       fontWeight: FontWeight.bold,
-                      fontSize: 18),
+                      fontSize: 18,
+                    ),
+                  ),
                 ),
-              ),
-            ],
-          ),
-
-          //Picture
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 36.0, vertical: 12),
-            child: Image.asset(imagePath),
-          ),
-          //Flavor
-          Text(
-            flavor,
-            style: TextStyle(
-                color: colors[800], fontWeight: FontWeight.bold, fontSize: 18),
-          ),
-          const SizedBox(
-            height: 4,
-          ),
-          Text(
-            'Bombón',
-            style: TextStyle(color: Colors.grey[600], fontSize: 16),
-          ),
-
-          //Love Icon and add button
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Icon(
-                  Icons.favorite,
-                  color: Colors.pink[400],
-                ),
-                Icon(
-                  Icons.shopping_cart,
-                  color: Colors.grey[800],
-                )
               ],
             ),
-          )
-        ]),
+
+            //Expanded Widget para la imagen
+            Expanded(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 36.0, vertical: 12),
+                child: Image.asset(imagePath),
+              ),
+            ),
+
+            //Flavor
+            Text(
+              flavor,
+              style: TextStyle(
+                color: colors[800],
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Text(
+              'Bombón',
+              style: TextStyle(color: Colors.grey[600], fontSize: 16),
+            ),
+
+            //Love Icon y botón de agregar
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(
+                    Icons.favorite,
+                    color: Colors.pink[400],
+                  ),
+                  Icon(
+                    Icons.shopping_cart,
+                    color: Colors.grey[800],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
